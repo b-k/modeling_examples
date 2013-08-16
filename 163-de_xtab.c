284b897eb771e59b5ca4e9e2b9494135234c22e6
Ben <ben@klemens.org>
2013-08-16 14:27:53 -0400

    comment out code that will work in the future

--- 163-de_xtab.c
+++ 163-de_xtab.c
@@ -2,7 +2,7 @@
 
 int main(){
     apop_data *d = apop_text_to_data("163-xtab",.delimiters=" ",.has_row_names='y', .has_col_names='y');
-    apop_data_show(d);
+    apop_data_print(d);
 
     //de-crosstab into the database. Arguments are names for the db.
     apop_crosstab_to_db(d, "wbdata", "country", "year", "expectancy");
@@ -11,7 +11,9 @@ int main(){
     sprintf(apop_opts.db_name_column, "country");
     apop_data_print(apop_query_to_data("select * from wbdata"), .output_file="unxtabbed");
     
+/*   oops: getting ahead of myself. Apop_matrix_row_t will be added shortly.
     printf("\nWhile we're here, a moving average for Algeria:\n");
-    Apop_matrix_row_t(d, "Algeria", af);
+    Apop_matrix_row_t(d->matrix, "Algeria", af);
     apop_vector_show(apop_vector_moving_average(af,2));
+    */
 }
